a9e34adfbb84a83da03aa50e422d818d1675d238
Jositoyoyo <jositoyoyo2@hotmail.com>
2013-12-11 16:35:01 +0100

    Firts

---
+++ jqueryDemos/ajax/Json.php
@@ -0,0 +1,4 @@
+<?php
+$arr = array('gato',"perro",'pajaro','raton');
+echo json_encode($arr);
+?>
---
+++ jqueryDemos/ajax/feeds.php
@@ -0,0 +1,13 @@
+<?php
+$valores =
+array
+(
+    "foo" => "bar",
+    "bar" => "foo",
+	"alpha" => "omega",
+);
+foreach($valores as $valor)
+{
+	echo $valor.'<br>';
+}
+?>
